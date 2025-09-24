bd786f8e75726660e2b0fff2fdcef7216ebf70e0
Botir Khaltaev <btrghstk@gmail.com>
2025-09-24 16:30:42 +0100

    feat(ci): add CI workflow for automated testing, linting, and building processes; enhance blog page metadata generation with async support
    chore: update dependencies to latest versions and remove unused nav item

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -11,8 +11,9 @@ export async function generateStaticParams() {
 	}));
 }
 
-export function generateMetadata({ params }) {
-	const post = getBlogPosts().find((post) => post.slug === params.slug);
+export async function generateMetadata({ params }) {
+	const { slug } = await params;
+	const post = getBlogPosts().find((post) => post.slug === slug);
 	if (!post) {
 		return;
 	}
@@ -51,8 +52,9 @@ export function generateMetadata({ params }) {
 	};
 }
 
-export default function Blog({ params }) {
-	const post = getBlogPosts().find((post) => post.slug === params.slug);
+export default async function Blog({ params }) {
+	const { slug } = await params;
+	const post = getBlogPosts().find((post) => post.slug === slug);
 
 	if (!post) {
 		notFound();
